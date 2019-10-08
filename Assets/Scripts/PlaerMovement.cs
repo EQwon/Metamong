3a9b4e244d06995c636bf104b1dd0f89386c7c20
EQwon <bluetwintail0906@gmail.com>
2019-10-09 00:32:38 +0900

    Sunny Land를 이용해서 기본 조작 구현

---
+++ Assets/Scripts/PlaerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaerMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private Animator animator;
+    public float speed = 30f;
+
+    private float horizontalMove = 0;
+    private bool jump = false;
+    private bool crouch = false;
+
+    private void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("IsCrouching", isCrouching);
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove, crouch, jump);
+        jump = false;
+    }
+}
