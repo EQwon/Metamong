e377e980343e2098ec5b2347fbd46c3fe083ba47
EQwon <bluetwintail0906@gmail.com>
2019-10-20 17:33:59 +0900

    간단하게 프롤로그 삽입

---
+++ Assets/Scripts/PrologueController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrologueController : MonoBehaviour
+{
+    [Header("Resources Holder")]
+    [SerializeField] private List<Sprite> prologueImages;
+    [SerializeField] private List<string> prologueSpeeches;
+
+    [Header("UI Holder")]
+    [SerializeField] private Image prologueImagePanel;
+    [SerializeField] private Text prologueText;
+
+    private int nowNum = 0;
+
+    private void Start()
+    {
+        ShowNowPrologue();
+    }
+
+    public void NextPrologue()
+    {
+        int targetNum = nowNum + 1;
+
+        if (targetNum >= prologueImages.Count)
+        {
+            // 프롤로그 끝
+            return;
+        }
+
+        nowNum = targetNum;
+        ShowNowPrologue();
+    }
+
+    private void ShowNowPrologue()
+    {
+        prologueImagePanel.sprite = prologueImages[nowNum];
+        prologueText.text = prologueSpeeches[nowNum];
+    }
+}
