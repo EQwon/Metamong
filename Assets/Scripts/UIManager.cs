e710fcfb4afa8e1444b1ae37c83dfebca8509fb3
EQwon <bluetwintail0906@gmail.com>
2019-10-29 23:26:30 +0900

    메뉴 패널 추가

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private RectTransform healthBar;
+    [SerializeField] private GameObject pausePanel;
 
     private GameObject player;
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<PlayerInput>().UI = this;
+
+        pausePanel.SetActive(false);
     }
 
     private void Update()
@@ -19,5 +24,15 @@ public class UIManager : MonoBehaviour
         Vector3 targetScale = Vector3.one;
         targetScale.x = player.GetComponent<PlayerInput>().HealthRatio;
         healthBar.localScale = targetScale;
+    }
+
+    public void MoveToTitle()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Pause()
+    {
+        pausePanel.SetActive(true);
     }
 }
