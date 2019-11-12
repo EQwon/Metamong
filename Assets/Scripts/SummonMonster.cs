18702ccff52516f1261dc9125ce04ca798e4759c
EQwon <bluetwintail0906@gmail.com>
2019-11-13 00:09:05 +0900

    소환진 기초 구현

---
+++ Assets/Scripts/SummonMonster.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummonMonster : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> monsterPrefabs;
+    [SerializeField] [Range(0, 2)] private int monsterType;
+
+    private void Start()
+    {
+        StartCoroutine(Summon());
+    }
+
+    private IEnumerator Summon()
+    {
+        yield return new WaitForSeconds(1);
+
+        Instantiate(monsterPrefabs[monsterType], transform.position, Quaternion.identity);
+    }
+}
