e7f67bae7018c9c3bfcd95aa079cdfdefdafb648
EQwon <bluetwintail0906@gmail.com>
2019-12-04 18:26:13 +0900

    챕터 2 보스 수정

---
+++ Assets/Scripts/Monster/BossAI_Chapter2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAI_Chapter2 : BossAI
+{
+    protected override IEnumerator Claw()
+    {
+        ClawPlayer();
+        yield return new WaitForSeconds(1f);
+
+        ClawPlayer();
+        yield return new WaitForSeconds(1f);
+
+        ClawPlayer();
+        yield return new WaitForSeconds(4f);
+
+        NextPattern();
+    }
+
+    private void ClawPlayer()
+    {
+        Vector2 attackPos = PlayerStatus.instance.transform.position;
+        GameObject nowClaw = Instantiate(claw, attackPos, Quaternion.identity);
+        ClawAttack clawAttack = nowClaw.GetComponent<ClawAttack>();
+        clawAttack.AttackPos = attackPos;
+        clawAttack.AttackSize = clawSize;
+        clawAttack.Direction = (attackPos.x >= transform.position.x) ? ClawDirection.Right : ClawDirection.Left;
+    }
+}
